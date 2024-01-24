349e26cb7f1f37e4f17b402764549a7d83e75318
AI-ANK <130707158+AI-ANK@users.noreply.github.com>
2024-01-24 13:22:19 +0000

    added code generatoor

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from llama_index.node_parser import SentenceSplitter, CodeSplitter, SemanticSpli
 from llama_index.node_parser.file import HTMLNodeParser, JSONNodeParser, MarkdownNodeParser
 from llama_index.vector_stores import FaissVectorStore, MilvusVectorStore, QdrantVectorStore
 
-@st.cache_data(experimental_allow_widgets=True) 
 def upload_file():
     file = st.file_uploader("Upload a file")
     if file is not None:
@@ -64,7 +63,7 @@ def select_llm():
     elif llm_choice == "Cohere":
         llm = Cohere(model="command", api_key=st.secrets['COHERE_API_TOKEN'])
         st.write(f"{llm_choice} selected")
-    return llm
+    return llm, llm_choice
 
 def select_embedding_model():
     st.header("Choose Embedding Model")
@@ -86,7 +85,7 @@ def select_embedding_model():
         st.session_state['embed_model'] = embed_model
         st.write(F"Embedding model selected: {embed_model}")
 
-    return embed_model
+    return embed_model, selected_model
 
 def select_node_parser():
     st.header("Choose Node Parser")
@@ -134,7 +133,7 @@ def select_node_parser():
         chunk_overlap = st.number_input("Chunk Overlap", min_value=0, value=20)
         parser = TokenTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)        
 
-    return parser
+    return parser, parser_type
 
 
 def select_response_synthesis_method():
@@ -150,7 +149,7 @@ def select_response_synthesis_method():
     ]
     selected_mode = st.selectbox("Select Response Mode", response_modes)
     response_mode = selected_mode
-    return response_mode
+    return response_mode, selected_mode
 
 def select_vector_store():
     st.header("Choose Vector Store")
@@ -169,10 +168,9 @@ def select_vector_store():
     elif selected_store == "Qdrant":
         client = qdrant_client.QdrantClient(location=":memory:")
         vector_store = QdrantVectorStore(client=client, collection_name="sampledata")
+    st.write(selected_store)
+    return vector_store, selected_store
 
-    return vector_store
-
-@st.cache_data
 def generate_rag_pipeline(file, llm, embed_model, node_parser, response_mode, vector_store):
     if vector_store is not None:
         # Set storage context if vector_store is not None
@@ -203,6 +201,75 @@ def send_query():
     response = f"Response for the query: {query}"
     st.markdown(response)
 
+def generate_code_snippet(llm_choice, embed_model_choice, node_parser_choice, response_mode, vector_store_choice):
+    print("VECTOR STORRRRR")
+    print(vector_store_choice)
+    code_snippet = "from llama_index.llms import OpenAI, Gemini, Cohere\n"
+    code_snippet += "from llama_index.embeddings import HuggingFaceEmbedding\n"
+    code_snippet += "from llama_index import ServiceContext, VectorStoreIndex, StorageContext\n"
+    code_snippet += "from llama_index.node_parser import SentenceSplitter, CodeSplitter, SemanticSplitterNodeParser, TokenTextSplitter\n"
+    code_snippet += "from llama_index.node_parser.file import HTMLNodeParser, JSONNodeParser, MarkdownNodeParser\n"
+    code_snippet += "from llama_index.vector_stores import FaissVectorStore, MilvusVectorStore, QdrantVectorStore\n"
+    code_snippet += "import faiss, qdrant_client\n\n"
+
+    # LLM initialization
+    if llm_choice == "GPT-3.5":
+        code_snippet += "llm = OpenAI(temperature=0.1, model='gpt-3.5-turbo-1106')\n"
+    elif llm_choice == "GPT-4":
+        code_snippet += "llm = OpenAI(temperature=0.1, model='gpt-4-1106-preview')\n"
+    elif llm_choice == "Gemini":
+        code_snippet += "llm = Gemini(model='models/gemini-pro')\n"
+    elif llm_choice == "Cohere":
+        code_snippet += "llm = Cohere(model='command', api_key='<YOUR_API_KEY>')  # Replace <YOUR_API_KEY> with your actual API key\n"
+
+    # Embedding model initialization
+    code_snippet += f"embed_model = HuggingFaceEmbedding(model_name='{embed_model_choice}')\n\n"
+
+    # Node parser initialization
+    node_parsers = {
+        "SentenceSplitter": "SentenceSplitter(chunk_size=1024, chunk_overlap=20)",
+        "CodeSplitter": "CodeSplitter(language='python', chunk_lines=40, chunk_lines_overlap=15, max_chars=1500)",
+        "SemanticSplitterNodeParser": "SemanticSplitterNodeParser(buffer_size=1, breakpoint_percentile_threshold=95, embed_model=embed_model)",
+        "TokenTextSplitter": "TokenTextSplitter(chunk_size=1024, chunk_overlap=20)",
+        "HTMLNodeParser": "HTMLNodeParser(tags=['p', 'h1'])",  # Update tags as needed
+        "JSONNodeParser": "JSONNodeParser()",
+        "MarkdownNodeParser": "MarkdownNodeParser()"
+    }
+    code_snippet += f"node_parser = {node_parsers[node_parser_choice]}\n\n"
+
+    # Response mode
+    code_snippet += f"response_mode = '{response_mode}'\n\n"
+
+    # Vector store initialization
+    if vector_store_choice == "Faiss":
+        code_snippet += "d = 1536\n"
+        code_snippet += "faiss_index = faiss.IndexFlatL2(d)\n"
+        code_snippet += "vector_store = FaissVectorStore(faiss_index=faiss_index)\n"
+    elif vector_store_choice == "Milvus":
+        code_snippet += "vector_store = MilvusVectorStore(dim=1536, overwrite=True)\n"
+    elif vector_store_choice == "Qdrant":
+        code_snippet += "client = qdrant_client.QdrantClient(location=':memory:')\n"
+        code_snippet += "vector_store = QdrantVectorStore(client=client, collection_name='sampledata')\n"
+    elif vector_store_choice == "Simple":
+        code_snippet += "vector_store = None  # Simple in-memory vector store selected\n"
+
+    code_snippet += "\n# Finalizing the RAG pipeline setup\n"
+    code_snippet += "if vector_store is not None:\n"
+    code_snippet += "    storage_context = StorageContext.from_defaults(vector_store=vector_store)\n"
+    code_snippet += "else:\n"
+    code_snippet += "    storage_context = None\n\n"
+
+    code_snippet += "service_context = ServiceContext.from_defaults(llm=llm, embed_model=embed_model, node_parser=node_parser)\n\n"
+
+    code_snippet += "_file = 'path_to_your_file'  # Replace with the path to your file\n"
+    code_snippet += "vector_index = VectorStoreIndex.from_documents(documents=_file, storage_context=storage_context, service_context=service_context, show_progress=True)\n"
+    code_snippet += "if storage_context:\n"
+    code_snippet += "    vector_index.storage_context.persist(persist_dir='persist_dir')\n\n"
+
+    code_snippet += "query_engine = vector_index.as_query_engine(response_mode=response_mode, verbose=True)\n"
+
+    return code_snippet
+
 def main():
     st.title("RAG Tester Application")
 
@@ -210,21 +277,23 @@ def main():
     file = upload_file()
 
     # Select RAG components
-    llm = select_llm()
-    embed_model = select_embedding_model()
+    llm, llm_choice = select_llm()
+    embed_model, embed_model_choice = select_embedding_model()
     #embeddings = embed_model.get_text_embedding("Hello World!")
     #print(len(embeddings))
     #print(embeddings[:5])
 
-    node_parser = select_node_parser()
+    node_parser, node_parser_choice = select_node_parser()
     # Process nodes only if a file has been uploaded
     if file is not None:
         if node_parser:
             nodes = node_parser.get_nodes_from_documents(file)
             print(f"First node: {nodes[0].text}")
 
-    response_mode = select_response_synthesis_method()
-    vector_store = select_vector_store()
+    response_mode, response_mode_choice = select_response_synthesis_method()
+    vector_store, vector_store_choice = select_vector_store()
+    print("VECCTOR STORE CHOICE")
+    print(vector_store_choice)
 
     # Generate RAG Pipeline Button
     if file is not None:
@@ -233,7 +302,7 @@ def main():
             st.session_state['query_engine'] = query_engine
             st.session_state['pipeline_generated'] = True
             st.success("RAG Pipeline Generated Successfully!")
-    else:
+    elif file is None:
         st.error('Please upload a file')
 
 
@@ -246,6 +315,10 @@ def main():
                 st.markdown(response, unsafe_allow_html=True)
             else:
                 st.error("Query engine not initialized. Please generate the RAG pipeline first.")
+  
+    if file and st.button("Generate Code Snippet"):
+        code_snippet = generate_code_snippet(llm_choice, embed_model_choice, node_parser_choice, response_mode_choice, vector_store_choice)
+        st.code(code_snippet, language='python')
 
 if __name__ == "__main__":
     main()
